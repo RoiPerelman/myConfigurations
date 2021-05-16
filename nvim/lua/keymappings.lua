2aa0069c8b0241278912ca41d66584821f4aba76
roi perelman <roiperelman@rois-iMac-4.local>
2021-05-16 18:09:05 +0300

    wip

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -1,3 +1,61 @@
+local keymap = vim.api.nvim_set_keymap 
+
+-- noremap means no recursive mappings which means if we map x to y and y to z. x would still be mapped to y and not z
+
 -- arrows are for pussies
--- set leader key - must be first thing we load in the config!
-vim.api.nvim_set_keymap('n', '<Leader>f', ':Telescope find_files<CR>', {noremap = true, silent = true})
+keymap('n', '<Up>', '<NOP>', {noremap = true, silent = true})
+keymap('n', '<Down>', '<NOP>', {noremap = true, silent = true})
+keymap('n', '<Left>', '<NOP>', {noremap = true, silent = true})
+keymap('n', '<Right>', '<NOP>', {noremap = true, silent = true})
+
+-- i hate escape
+keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
+keymap('i', 'kk', '<ESC>', {noremap = true, silent = true})
+keymap('i', 'jk', '<ESC>', {noremap = true, silent = true})
+keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
+
+-- keymap('n', '', 'viwU<ESC>', {noremap = true, silent = true}) -- easy uppercase
+-- keymap('n', '', ':bnext', {noremap = true, silent = true}) -- move to next buffer
+-- keymap('n', '', ':bprevious', {noremap = true, silent = true}) -- move to previous buffer 
+-- TODO doesn't work
+keymap('n', '<C-s>', ':w<CR>', {noremap = true, silent = true }) -- alternate way to save
+
+keymap('v', '<', '<gv', {noremap = true, silent = true }) -- do not loose visual mode when indenting
+keymap('v', '>', '>gv', {noremap = true, silent = true }) -- do not loose visual mode when indenting
+
+-- " TODO better keymapss Use alt + hjkl to resize windows
+-- keymap('n', '<Leader>j', ':resize -2<CR>', {noremap = true, silent = true })
+-- keymap('n', '<Leader>k', ':resize +2<CR>', {noremap = true, silent = true })
+-- keymap('n', '<Leader>h', ':vertical resize -2<CR>', {noremap = true, silent = true })
+-- keymap('n', '<Leader>l', ':vertical resize +2<CR>', {noremap = true, silent = true })
+
+
+-- Telescope
+keymap('n', '<Leader>f', ':Telescope find_files<CR>', {noremap = true, silent = true})
+
+-- " Better window navigation
+-- "nnoremap <C-h> <C-w>h
+-- "nnoremap <C-j> <C-w>j
+-- "nnoremap <C-k> <C-w>k
+-- "nnoremap <C-l> <C-w>l
+
+
+-- " Better nav for omnicomplete
+-- "inoremap <expr> <c-j> ("\<C-n>")
+-- inoremap <expr><TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
+-- "inoremap <expr> <c-k> ("\<C-p>")
+
+-- " TAB in general mode will move to text buffer
+-- nnoremap <TAB> :bnext<CR>
+-- " SHIFT-TAB will go back
+--
+-- " <TAB>: completion.
+-- nnoremap <S-TAB> :bprevious<CR>
+ 
+-- " Move lines up and down
+vim.cmd([[nnoremap <A-Down> :m .+1<CR>==]])
+vim.cmd([[nnoremap <A-Up> :m .-2<CR>==]])
+vim.cmd([[inoremap <A-Down> <Esc>:m .+1<CR>==gi]])
+vim.cmd([[inoremap <A-Up> <Esc>:m .-2<CR>==gi]])
+vim.cmd([[vnoremap <A-Down> :m '>+1<CR>gv=gv]])
+vim.cmd([[vnoremap <A-Up> :m '<-2<CR>gv=gv]])
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -15,6 +15,9 @@ vim.o.smartindent = true -- makes indenting smart
 vim.o.showmode = false -- we don't need to see things like -- INSERT -- anymore
 vim.o.ignorecase = true -- ignore case in search
 vim.o.smartcase = true -- ignore case in search only if no capital letter
+vim.o.scrolloff = 1 -- scroll before getting to the last line
+vim.o.clipboard = "unnamedplus" -- always use the system clipboard
+-- vim.o.guifont = "" -- change font
 
 -- window options
 vim.wo.number = true
@@ -24,27 +27,8 @@ vim.wo.cursorline = true
 
 -- TODO fix how to set option from lua vim api and not vim.cmd
 vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object
-vim.cmd('set clipboard+=unnamedplus') -- always use the system clipboard
 
--- " => Vim Essentials - turned on when using vim
--- filetype on
--- filetype plugin on
--- filetype indent on
--- set ignorecase                          " Ignore case in serach
--- set smartcase                           " Ignore case in search only if not capital letter
--- set scrolloff=1                         " scroll before getting to last line
--- set lazyredraw                          " no redraw while executing macros (good preformace config)
--- set magic                               " better regex (keep it)
--- set ffs=unix,dos,mac                    " file format detection
--- 
--- set nobackup                            " This is recommended by coc
--- set nowritebackup                       " This is recommended by coc
--- set shortmess+=c                        " This is recommended by coc
--- set signcolumn=yes                      " This is recommended by coc
--- 
 -- " return to last edit position when opening files!
 -- au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
--- " auto source when writing to init.vm alternatively you can run :source $MYVIMRC
--- au! BufWritePost $MYVIMRC source %
 -- " You can't stop me
 -- cmap w!! w !sudo tee %
