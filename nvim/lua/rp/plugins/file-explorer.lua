6ed8de5f712b6af1e00aecf92365b94cba950fd2
roip <roip@inspekto.com>
2024-04-07 17:50:32 +0300

    nvim - update dependencies, comment out nvim tree

--- nvim/lua/rp/plugins/file-explorer.lua
+++ nvim/lua/rp/plugins/file-explorer.lua
@@ -1,20 +1,20 @@
 return {
   "stevearc/oil.nvim",
   config = function()
-    vim.keymap.set("n", "<Leader>e", ":Oil<CR>", { desc = "File [E]xplorer" })
     require("oil").setup({
       view_options = {
         show_hidden = true,
       },
     })
+    vim.keymap.set("n", "<Leader>e", ":Oil<CR>", { desc = "File [E]xplorer" })
   end,
   -- TODO: DECIDE if I want to remove
   -- nvim-tree as backup
-  {
-    "kyazdani42/nvim-tree.lua",
-    config = function()
-      vim.keymap.set("n", "<Leader>E", ":NvimTreeToggle<CR>", { desc = "File [E]xplorer Tree" })
-      require("nvim-tree").setup({})
-    end,
-  },
+  -- {
+  --   "kyazdani42/nvim-tree.lua",
+  --   config = function()
+  --     require("nvim-tree").setup({})
+  --     vim.keymap.set("n", "<Leader>E", ":NvimTreeToggle<CR>", { desc = "File [E]xplorer Tree" })
+  --   end,
+  -- },
 }
