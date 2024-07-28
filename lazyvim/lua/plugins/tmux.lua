310ca0834bec7d5b57403cced1ddd33f5feab535
Perelman Roi (DI FA) <roi-perelman@siemens.com>
2024-07-28 20:44:29 +0300

    layzvim: add tmux navigation

---
+++ lazyvim/lua/plugins/tmux.lua
@@ -0,0 +1,13 @@
+return {
+  "alexghergh/nvim-tmux-navigation",
+  event = "VimEnter",
+  keys = {
+    { "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", desc = "Go to Left Window" },
+    { "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", desc = "Go to Lower Window" },
+    { "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", desc = "Go to Upper Window" },
+    { "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", desc = "Go to Right Window" },
+  },
+  config = function()
+    require("nvim-tmux-navigation").setup({})
+  end,
+}
