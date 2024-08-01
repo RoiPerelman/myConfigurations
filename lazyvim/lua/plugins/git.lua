f9e2ca1b5cee197008a031f0fb931064ec2d0613
Perelman Roi (DI FA) <roi-perelman@siemens.com>
2024-08-01 16:58:37 +0300

    lazygit: remove neogit, blame and put fugitive

--- lazyvim/lua/plugins/git.lua
+++ lazyvim/lua/plugins/git.lua
@@ -1,23 +1,4 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    keys = {
-      { "<Leader>gg", "<cmd>Neogit<cr>", desc = "Neo[G]it Magit" },
-    },
-    config = function()
-      require("neogit").setup({})
-    end,
-  },
-  {
-    "FabijanZulj/blame.nvim",
-    config = function()
-      require("blame").setup()
-      vim.keymap.set("n", "<Leader>gb", "<cmd>BlameToggle<cr>", { desc = "[G]it [B]lame Line Toggle" })
-    end,
-  },
+  {"tpope/vim-fugitive"},
+  {"sindrets/diffview.nvim"},
 }
