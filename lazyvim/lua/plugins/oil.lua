33c78a95b9a6a8652f2eab6b34862992fe613f24
roiperelman <roiperelman@gmail.com>
2024-08-14 03:00:27 +0300

    lazyvim: add oil, bring back yanky with <c-n> <c-p>

---
+++ lazyvim/lua/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  opts = {
+      view_options = {
+        show_hidden = true,
+      },
+      keymaps = {
+        ["<C-v>"] = "actions.select_vsplit",
+      },
+  },
+  keys = {
+    { "<Leader>o", ":Oil<CR>", mode = { "n" }, desc = "Explorer Oil (Root Dir)" },
+  },
+}
--- lazyvim/lua/plugins/yanky-extension.lua
+++ lazyvim/lua/plugins/yanky-extension.lua
@@ -2,5 +2,18 @@ return {
   "gbprod/yanky.nvim",
   keys = {
     { "p", "\"_d<Plug>(YankyPutBefore)", mode = { "x" }, desc = "Put Text After Cursor" },
+    { "<c-n>", "<Plug>(YankyNextEntry)", desc = "Cycle Forward Through Yank History" },
+    { "<c-p>", "<Plug>(YankyPreviousEntry)", desc = "Cycle Backward Through Yank History" },
+    -- disable keys
+    { "]p", false },
+    { "[p", false },
+    { "]P", false },
+    { "[P", false },
+    { ">p", false },
+    { "<p", false },
+    { ">P", false },
+    { "<P", false },
+    { "=p", false },
+    { "=P", false },
   },
 }
