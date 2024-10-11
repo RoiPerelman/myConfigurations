c0d3e1c1ef9458c2dae732f05a908baf1a6e3b9e
Perelman Roi (DI FA) <roi-perelman@siemens.com>
2024-10-10 18:35:37 -0600

    rp-vertico wip

--- .config/nvim/lua/rp-vertico/init.lua
+++ .config/nvim/lua/rp-vertico/init.lua
@@ -1,20 +1,47 @@
 local Utils = require('rp-vertico.utils')
 
 local M = {}
+local H = {}
 
 function M.setup()
 end
 
+H.timers = {
+  getcharstr = vim.loop.new_timer(),
+}
+
+H.cache = {
+  is_in_getcharstr = nil,
+}
+
+local x = 0
+local y = 0
+function H.redraw_scheduled()
+  vim.schedule(function() -- Ensure this runs in the main event loop
+    vim.notify('redrawing ' .. tostring(y) .. ' ' .. tostring(x))
+    x = x + 1
+  end)
+end
+
 function M.toggle()
-  Utils.toggle_window_type_once('rp-vertico')
-  for _ = 1, 1000000 do
-    local ok, char = pcall(vim.fn.getcharstr)
-    print('ROIROI ok', ok)
-    print('ROIROI char', char)
-    if char == 'Q' then
-      break
+  local is_open = Utils.toggle_window_by_name('rp-vertico')
+  if is_open then
+    for _ = 1, 1000000 do
+      H.timers.getcharstr:start(0, 1000, H.redraw_scheduled)
+      H.cache.is_in_getcharstr = true
+      local ok, char = pcall(vim.fn.getcharstr)
+      H.cache.is_in_getcharstr = nil
+      H.timers.getcharstr:stop()
+      y = y + 1
+      print('ROIROI char', char)
+      if not ok or char == 'Q' then
+        Utils.toggle_window_by_name('rp-vertico')
+        break
+      end
     end
   end
 end
 
 return M
+
+--
--- .config/nvim/lua/rp-vertico/utils.lua
+++ .config/nvim/lua/rp-vertico/utils.lua
@@ -1,8 +1,13 @@
 local U = {}
 
 U.cache = {
-  creating_window = {},
-  alternate_window = nil
+  managed_windows = {
+    ['rp-vertico'] = {
+      buffer = nil,
+      window = nil,
+      alternate_window = nil,
+    },
+  },
 }
 
 function U.get_scratch_buffer(bufname)
@@ -29,33 +34,34 @@ function U.display_buffer(bufnr)
   vim.api.nvim_win_set_option(new_win, "winfixheight", true)
   vim.api.nvim_win_set_option(new_win, "wrap", true)
   vim.api.nvim_win_set_height(new_win, 11)
+  vim.api.nvim_win_set_cursor(new_win, { 1, 0 })
+  return new_win
 end
 
-U.toggle_window_type_once = function(name)
-  local bufnr = U.get_scratch_buffer(name)
-  local windows = vim.fn.win_findbuf(bufnr)
+U.toggle_window_by_name = function(name)
+  local buffer = U.get_scratch_buffer(name)
+  local windows = vim.fn.win_findbuf(buffer)
   -- Buffer not displayed in any window
   if #windows == 0 then
-    if not U.cache.creating_window[name] then
-      U.cache.creating_window[name] = true
-      local current_win = vim.api.nvim_get_current_win()
-      U.cache.alternate_window = current_win
-      vim.print('ROIROI 0000', U.cache)
-      vim.schedule(function()
-        U.display_buffer(bufnr)
-        U.cache.creating_window[name] = false
-      end)
-    end
+    -- vim.schedule(function()
+    U.cache.managed_windows[name].alternate_window = vim.api.nvim_get_current_win()
+    U.cache.managed_windows[name].window = U.display_buffer(buffer)
+    U.cache.managed_windows[name].buffer = buffer
+    -- end)
+    return true
   else
-    vim.print('ROIROI 1111', U.cache)
     -- Return focus to the original window without making the new one the alternate window
-    if U.cache.alternate_window then
-      vim.api.nvim_set_current_win(U.cache.alternate_window)
+    if U.cache.managed_windows[name].alternate_window then
+      vim.api.nvim_set_current_win(U.cache.managed_windows[name].alternate_window)
     end
     -- Buffer is displayed, so close all windows showing it
-    for _, win in ipairs(windows) do
-      vim.api.nvim_win_close(win, true)
+    for _, window in ipairs(windows) do
+      vim.api.nvim_win_close(window, true)
     end
+    -- clear cache
+    U.cache.managed_windows[name].alternate_window = nil
+    U.cache.managed_windows[name].window = nil
+    return false
   end
 end
 
