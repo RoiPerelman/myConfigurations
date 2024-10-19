539ecd08509d95eda9da20426259401b87345d0b
Perelman Roi (DI FA) <roi-perelman@siemens.com>
2024-10-19 17:44:38 +0300

    nvim: rp-vertico wip - start with extmark

--- .config/nvim/lua/rp-vertico/init.lua
+++ .config/nvim/lua/rp-vertico/init.lua
@@ -1,3 +1,4 @@
+local Window = require('rp-vertico.window')
 local Utils = require('rp-vertico.utils')
 
 local M = {}
@@ -6,9 +7,28 @@ local H = {}
 function M.setup()
 end
 
+M.actions = {
+  stop = function()
+  end,
+  caret_left = function()
+    vim.notify('RP amount of items' .. tostring(#H.cache.query))
+    vim.notify('RP placem of caret' .. tostring(H.cache.caret))
+    if H.cache.caret > 1 then
+      H.cache.caret = H.cache.caret - 1
+    end
+  end,
+  caret_right = function()
+    vim.notify('RP amount of items' .. tostring(#H.cache.query))
+    vim.notify('RP placem of caret' .. tostring(H.cache.caret))
+    if H.cache.caret <= #H.cache.query then
+      H.cache.caret = H.cache.caret + 1
+    end
+  end
+}
+
 M.actions_map = {
-  ['<Left>']    = 'caret_left',
-  ['<Right>']   = 'caret_right',
+  ['<Left>']    = M.actions.caret_left,
+  ['<Right>']   = M.actions.caret_right,
 
   ['<CR>']      = 'choose',
   ['<C-s>']     = 'choose_in_split',
@@ -45,11 +65,6 @@ M.actions_map = {
   ['<Tab>']     = 'toggle_preview',
 }
 
-H.actions = {
-  stop = function()
-  end
-}
-
 H.timers = {
   redraw = vim.loop.new_timer(),
 }
@@ -63,9 +78,32 @@ H.cache = {
   items = {},
   caret = 1,
   current_ind = nil,
+  guicursor = '',
 
   -- caret = {}
 }
+
+H.ns_id = vim.api.nvim_create_namespace('RPVertico')
+
+function H.create_hl_groups()
+  vim.api.nvim_set_hl(0, 'RPVerticoCursor', { link = 'Cursor' })
+  vim.api.nvim_set_hl(0, 'RPVerticoCursorLine', { link = 'CursorLine' })
+  vim.api.nvim_set_hl(0, 'RPVerticoMarkedLine', { link = 'Visual' })
+end
+
+function H.set_win_options()
+  local window = Window.cache.managed_windows['rp-vertico'].window
+  vim.wo[window].foldenable = false
+  vim.wo[window].foldmethod = 'manual'
+  vim.wo[window].list = true
+  vim.wo[window].listchars = 'extends:…'
+  vim.wo[window].scrolloff = 0
+  vim.wo[window].wrap = false
+  vim.wo[window].number = false
+  vim.wo[window].relativenumber = false
+  vim.wo[window].signcolumn = 'no'
+  -- vim.wo[window].cursorline = false
+end
 
 function H.replace_termcodes(actions)
   local replaced_actions = {}
@@ -78,25 +116,66 @@ end
 
 function H.redraw_scheduled()
   vim.schedule(function()
-    buffer = Utils.cache.managed_windows['rp-vertico'].buffer
-    vim.print(buffer)
-    query = table.concat(H.cache.query, "")
+    local buffer = Window.cache.managed_windows['rp-vertico'].buffer
+    if buffer == nil then
+      return
+    end
 
     local current = H.cache.current_ind and tostring(H.cache.current_ind) or '!'
     local total = tostring(#H.cache.items)
+    local position = current .. '/' .. total
+
+    local before_caret = table.concat(vim.list_slice(H.cache.query, 1, H.cache.caret - 1), '')
+    local after_caret = table.concat(vim.list_slice(H.cache.query, H.cache.caret, #H.cache.query), '')
+
+    vim.notify('ROIROI total' .. tostring(total))
 
     vim.api.nvim_buf_set_lines(buffer, 0, 1, false,
-      { current .. '/' .. total .. ' ' .. query })
-    vim.notify(vim.inspect(H.cache.items))
+      { position .. ' ' .. before_caret .. after_caret .. ' ' })
+    vim.notify('ROIROI 111')
     vim.api.nvim_buf_set_lines(buffer, 1, -1, false, H.cache.items)
+
+    vim.notify('ROIROI 222')
+    vim.api.nvim_buf_clear_namespace(buffer, -1, 1, 2)
+    vim.notify('ROIROI 333')
+    vim.api.nvim_buf_add_highlight(buffer, H.ns_id, 'RPVerticoCursor', 0, H.cache.caret + 3, H.cache.caret + 4)
+    vim.notify('ROIROI 444')
+
+
+    Utils.set_extmark(buffer, H.ns_id, 4, 0, {
+      hl_group = 'RPVerticoCursorLine',
+      end_row = 5,
+      end_col = 0,
+      hl_eol = true,
+      priority = 300,
+    })
+
+    vim.notify('ROIROI 555')
     vim.cmd('redraw')
   end)
 end
 
+function H.hide_cursor()
+  -- Hide cursor while picker is active (to not be visible in the window)
+  -- This mostly follows a hack from 'folke/noice.nvim'
+  H.cache.guicursor = vim.o.guicursor
+  vim.api.nvim_set_hl(0, 'RPVerticoNoGuiCursor', { blend = 100, nocombine = true, default = true })
+  vim.o.guicursor = 'a:RPVerticoNoGuiCursor'
+end
+
+function H.unhide_cursor()
+  -- bring back cursor
+  if H.cache.guicursor == '' then vim.cmd('set guicursor=a: | redraw') end
+  pcall(function() vim.o.guicursor = H.cache.guicursor end)
+end
+
 function M.open()
-  local is_open = Utils.toggle_window_by_name('rp-vertico')
+  local is_open = Window.toggle_window_by_name('rp-vertico')
   if is_open then
     H.cache.query = {}
+    H.create_hl_groups()
+    H.set_win_options()
+    H.hide_cursor()
     M.main_loop()
   end
 end
@@ -114,18 +193,24 @@ function M.main_loop()
     H.timers.redraw:stop()
 
     local c = vim.api.nvim_replace_termcodes('<C-z>', true, true, true)
-    if actions_map[char] then
+    if type(actions_map[char]) == "function" then
+      actions_map[char]()
+      goto continue
+    elseif actions_map[char] then
       vim.notify('special char ' .. char .. ' ' .. actions_map[char])
       goto continue
     end
 
     -- close using C-c
     if not ok then
-      Utils.toggle_window_by_name('rp-vertico')
+      Window.toggle_window_by_name('rp-vertico')
+      H.unhide_cursor()
       break
     end
 
-    table.insert(H.cache.query, char)
+    table.insert(H.cache.query, H.cache.caret, char)
+    H.cache.caret = H.cache.caret + 1
+    vim.print(vim.inspect(H.cache.query))
 
     ::continue::
   end
@@ -158,7 +243,6 @@ function M.find_files()
     data_feed = nil
     -- and close the pipe
     stdout:close()
-    vim.print('ROIROI updated cache')
     H.cache.items = items
     H.redraw_scheduled()
   end)
---
+++ .config/nvim/lua/rp-vertico/window.lua
@@ -0,0 +1,68 @@
+local U = {}
+
+U.cache = {
+  managed_windows = {
+    ['rp-vertico'] = {
+      buffer = nil,
+      window = nil,
+      alternate_window = nil,
+    },
+  },
+}
+
+function U.get_scratch_buffer(bufname)
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.fn.bufname(bufnr) == bufname then
+      return bufnr
+    end
+  end
+
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_name(bufnr, bufname)
+  vim.api.nvim_buf_set_option(bufnr, "buftype", "nofile")
+  vim.api.nvim_buf_set_option(bufnr, "bufhidden", "hide")
+  vim.api.nvim_buf_set_option(bufnr, "swapfile", false)
+
+  return bufnr
+end
+
+function U.display_buffer(bufnr)
+  vim.cmd("botright new")
+  local new_win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(new_win, bufnr)
+  -- Make sure it doesn't affect layout by preserving 'winfixwidth'
+  vim.api.nvim_win_set_option(new_win, "winfixheight", true)
+  vim.api.nvim_win_set_option(new_win, "wrap", true)
+  vim.api.nvim_win_set_height(new_win, 11)
+  vim.api.nvim_win_set_cursor(new_win, { 1, 0 })
+  return new_win
+end
+
+U.toggle_window_by_name = function(name)
+  local buffer = U.get_scratch_buffer(name)
+  local windows = vim.fn.win_findbuf(buffer)
+  -- Buffer not displayed in any window
+  if #windows == 0 then
+    -- vim.schedule(function()
+    U.cache.managed_windows[name].alternate_window = vim.api.nvim_get_current_win()
+    U.cache.managed_windows[name].window = U.display_buffer(buffer)
+    U.cache.managed_windows[name].buffer = buffer
+    -- end)
+    return true
+  else
+    -- Return focus to the original window without making the new one the alternate window
+    if U.cache.managed_windows[name].alternate_window then
+      vim.api.nvim_set_current_win(U.cache.managed_windows[name].alternate_window)
+    end
+    -- Buffer is displayed, so close all windows showing it
+    for _, window in ipairs(windows) do
+      vim.api.nvim_win_close(window, true)
+    end
+    -- clear cache
+    U.cache.managed_windows[name].alternate_window = nil
+    U.cache.managed_windows[name].window = nil
+    return false
+  end
+end
+
+return U
