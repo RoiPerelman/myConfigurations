73a606602f3447a5479eeb4af5c3983894f17069
Perelman Roi (DI FA) <roi-perelman@siemens.com>
2025-04-22 15:37:33 -0600

    emacs - wip

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -1,3 +1,10 @@
+(require 'package)
+;; to make sure we are up to date (package-refresh-contents)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
 (use-package emacs
   :init
   ;; Disable startup message
@@ -67,6 +74,24 @@
 	`((".*" ,(concat user-emacs-directory "saves") t)))
 )
 
+;; (set-face-attribute 'variable-pitch nil
+;; 		    :family "Ubuntu"
+;; 		    :weight 'semi-bold
+;; 		    :height 120)
+;; (set-face-attribute 'fixed-pitch nil
+;; 		    :family "Jetbrains Mono"
+;; 		    :weight 'normal
+;; 		    :height 100)
+;; (set-face-attribute 'default nil
+;; 		    :family "Jetbrains Mono"
+;; 		    :weight 'normal
+;; 		    :height 110)
+;; ;; (add-to-list 'default-frame-alist '(font . "JetBrains Mono 14"))
+;; (set-face-attribute 'font-lock-comment-face nil :slant 'italic)
+;; (set-face-attribute 'font-lock-function-name-face nil :slant 'italic)
+;; (set-face-attribute 'font-lock-variable-name-face nil :slant 'italic)
+;; (set-face-attribute 'font-lock-keyword-face nil :slant 'italic)
+
 ;; to see colors M-x modus-themes-list-colors-current
 ;; to see original palette C-h f Modus-vivendi-palette
 ;; to see character info under the point - M-x describe-char
@@ -84,6 +109,36 @@
 	  ;; (comment red-intense)
 	  ))
   :config (load-theme 'modus-vivendi))
+
+(defun toggle-comment-on-line ()
+  "Comment or uncomment current line."
+  (interactive)
+  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+
+(use-package emacs
+  :init
+  ;; Set up keybindings for config workflow
+  ;; Make esc work like C-g
+  (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+  ;; `C-x o' is a 2 step key binding. `M-o' is much easier.
+  (global-set-key (kbd "M-o") 'other-window)
+  (global-set-key (kbd "C-;") 'toggle-comment-on-line)
+  (global-set-key (kbd "M-k") 'kill-current-buffer)
+
+  ;; zoom in and out
+  (global-set-key (kbd "C-=") 'text-scale-increase)
+  (global-set-key (kbd "C--") 'text-scale-decrease)
+  (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+  (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+
+  (global-set-key (kbd "M-s M-r")
+    (lambda () (interactive) (load-file "~/.config/emacs/init.el")))
+  (global-set-key (kbd "M-s M-c")
+    (lambda () (interactive) (find-file "~/.config/emacs/config.org")))
+  )
+
+;; Delete whitespace just when a file is saved.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; save minibuffer histories. Vertico uses to put recently selected options at the top.
 (savehist-mode 1)
@@ -103,5 +158,100 @@
   :hook
   (marginalia-mode . all-the-icons-completion-marginalia-setup)
   :config
-  ;; (setq marginalia-align 'right)
+  (setq marginalia-align 'right)
   (marginalia-mode))
+
+;; Gives enhanced completion functions we need to bind
+;; Gives previews for current item
+;; binds M-s as opposed to native C-s C-r
+(use-package consult
+  :bind (;; A recursive grep
+         ("M-s M-g" . consult-ripgrep)
+	 ("M-s M-G" . consult-grep)
+         ;; Search for files names recursively
+         ("M-s M-f" . consult-fd)
+	 ("M-s M-F" . consult-find)
+         ;; Search through the outline (headings) of the file
+         ("M-s M-o" . consult-outline)
+         ;; Search the current buffer
+         ("M-s M-l" . consult-line)
+         ;; Switch to another buffer/bookmarked/recent file.
+         ("M-s M-b" . consult-buffer)
+	 ;; search on imenu
+	 ("M-s M-i" . consult-imenu)
+	 ;; change theme
+	 ("M-s M-t" . consult-theme)
+	 ;; search mark
+	 ("M-s M-m" . consult-mark)
+	 ;; search help info
+	 ("M-s M-h" . consult-info)
+	 )
+  :config
+  ;; Use `consult-completion-in-region' if Vertico is enabled.
+  ;; Otherwise use the default `completion--in-region' function.
+  (setq completion-in-region-function
+	(lambda (&rest args)
+	  (apply (if vertico-mode
+		     #'consult-completion-in-region
+		   #'completion--in-region)
+		 args)))
+
+  (defvar consult-rp-project-files-source
+    (list :name     "Project Files"
+          :category 'file
+          :narrow   ?p
+          :face     'consult-file
+          :history  'file-name-history
+          :state    #'consult--file-state
+	  ;; :action   ,(lambda (file)
+	  ;; 	       (consult--file-action
+	  ;; 		(expand-file-name file (project-root (project-current)))))
+          :items
+	  ;; TODO: shorten the name and color me different
+	  (lambda ()
+	    (when-let* ((project (project-current))
+			(root (project-root project)))
+	      ;; (mapcar (lambda (file) (file-relative-name file root))
+	      (project-files project)))))
+
+  (add-to-list 'consult-buffer-sources 'consult-rp-project-files-source 'append)
+  )
+
+;; adds actions for current item
+(use-package embark
+  :bind (("C-." . embark-act)
+         :map minibuffer-local-map
+         ("C-c C-c" . embark-collect)
+         ("C-c C-e" . embark-export)))
+
+;; adds embark actions to consult functions
+(use-package embark-consult
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
+;; edit the results of a grep search  while inside a `grep-mode' buffer.
+;; toggle editable mode, make changes, type C-c C-c to confirm | C-c C-k to abort.
+(use-package wgrep
+  :bind ( :map grep-mode-map
+          ("e" . wgrep-change-to-wgrep-mode)
+          ("C-x C-q" . wgrep-change-to-wgrep-mode)
+          ("C-c C-c" . wgrep-finish-edit)))
+
+(use-package toc-org
+  :ensure t
+  :commands toc-org-enable
+  :init (add-hook 'org-mode-hook 'toc-org-enable))
+(electric-indent-mode -1)
+(require 'org-tempo)
+
+ (add-hook 'org-mode-hook 'org-indent-mode)
+ (use-package org-bullets
+   :ensure t
+   :config
+   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+   )
+
+(use-package sudo-edit
+  :ensure t
+  :config
+  (global-set-key (kbd "C-c f u") #'sudo-edit-find-file)
+  (global-set-key (kbd "C-c f U") #'sudo-edit))
