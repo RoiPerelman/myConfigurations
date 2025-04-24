7b2cae2d933102c840fbd08cf4a8c789dda0c0ca
Perelman Roi (DI FA) <roi-perelman@siemens.com>
2025-04-24 10:10:41 -0600

    emacs - wip#3

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -6,78 +6,169 @@
   (package-refresh-contents))
 
 (use-package emacs
+  :ensure nil
   :init
-  ;; Disable startup message
-  (setq inhibit-startup-screen t)
-  ;; Disable annoying noisy bell
-  (setq ring-bell-function 'ignore)
-  ;; Disable visible scrollbar
-  (scroll-bar-mode -1)
-  ;; Disable the toolbar
-  (tool-bar-mode -1)
-  ;; Disable tooltips
-  (tooltip-mode -1)
-  ;; Disable the menu bar
-  (menu-bar-mode -1)
-  ;; Add some space to lines
-  (set-fringe-mode 5)
-  ;; Highlight current line
-  (global-hl-line-mode t)
-  ;; Automatically update buffers if file content on the disk has changed.
-  (global-auto-revert-mode t)
-  ;; automatically delete selected text without backspace
-  (delete-selection-mode 1)
-  ;; Change all yes/no questions to y/n type (fset 'yes-or-no-p 'y-or-n-p)
-  (setq use-short-answers t)
-  ;; for dired
-  (setq dired-dwim-target t)
+  (setq inhibit-startup-screen t) 	; Disable startup message
+  (setq ring-bell-function 'ignore)	; Disable annoying noisy bell
+  (scroll-bar-mode -1)			; Disable visible scrollbar
+  (tool-bar-mode -1)			; Disable the toolbar
+  (tooltip-mode -1)			; Disable tooltips
+  (menu-bar-mode -1)			; Disable the menu bar
+  (set-fringe-mode 5)			; Add some space to lines
+  (global-hl-line-mode t)		        ; Highlight current line
+  (global-auto-revert-mode t)             ; Automatically update buffers if file changes on disk
+  (delete-selection-mode 1)               ; Automatically delete selected text without backspace
+  (setq use-short-answers t)		; Use y/n instead of yes/no
+  (setq dired-dwim-target t)              ; Enable smarter target suggestion in dired
+  )
 
-  ;; Show line numbers
-  (global-display-line-numbers-mode 1)
-  ;; Show column as well as line number in bottom line
-  (column-number-mode 1)
+(use-package emacs
+  :ensure nil
+  :init
+  (global-display-line-numbers-mode 1)	; Show line numbers
+  (column-number-mode 1)                  ; Show column as well as line number in bottom line
+
   ;; Disable line numbers for some modes
   (dolist (mode '(
-  		org-mode-hook
-  		term-mode-hook
-  		shell-mode-hook
-  		eshell-mode-hook
-  		vterm-mode-hook
-  		))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+    		  org-mode-hook
+    		  term-mode-hook
+    		  shell-mode-hook
+    		  eshell-mode-hook
+    		  vterm-mode-hook
+    		  ))
+    (add-hook mode (lambda () (display-line-numbers-mode 0))))
+  )
+
+(use-package emacs
+  :ensure nil
+  :init
+  ;; make sure we start emacs fullscreen and maximized
+  (set-frame-parameter (selected-frame) 'fullscreen 'maximized)	; sets initial frame
+  (add-to-list 'default-frame-alist '(fullscreen . maximized))    ; sets next frames
+  )
+
+(use-package emacs
+  :ensure nil
+  :init
+  (setq custom-file (concat user-emacs-directory "custom.el")) ; set custom file - so things wont be added in this file
+  (load custom-file 'noerror)
+
+  ;; set backup directory (Use copying to avoid symlinks)
+  (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+        backup-by-copying t
+        delete-old-versions t
+        kept-new-versions 6
+        kept-old-versions 2
+        version-control t)
+  (setq auto-save-file-name-transforms
+        `((".*" ,(concat user-emacs-directory "saves") t)))
+  )
+
+(use-package emacs
+  :ensure nil
+  :init
   (when (eq system-type 'darwin)
     ;; Change meta from option to command key
     (setq mac-option-key-is-meta nil
           mac-command-key-is-meta t
           mac-command-modifier 'meta
           mac-option-modifier 'none))
-    (use-package exec-path-from-shell
-      :if (memq window-system '(mac ns x))
-      :ensure t
-      :config
-      (exec-path-from-shell-initialize))
 
-  ;; make sure we start emacs fullscreen and maximized
-  ;; sets initial frame
-  (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
-  ;; sets next frames
-  (add-to-list 'default-frame-alist '(fullscreen . maximized))
+  ;; make it so starting emacs as an app, actually use the shell for env variables
+  (use-package exec-path-from-shell
+    :if (memq window-system '(mac ns x))
+    :ensure t
+    :config
+    (exec-path-from-shell-initialize))
+  )
 
-  ;;; ───────────────────────── 'No-littering' ────────────────────────
-  ;; set custom file - so things wont be added in this file
-  (setq custom-file (concat user-emacs-directory "custom.el"))
-  (load custom-file 'noerror)
+;; update isearch functionality
+(use-package isearch
+  :ensure nil
+  :defer t
+  :config
+  (setq isearch-lazy-count t)	   ; adds a counter eg 4/34 to isearch
+  ;; use selection to search (https://www.reddit.com/r/emacs/comments/2amn1v/comment/cixq7zx/)
+  (defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
+    (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
+        (progn
+          (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
+          (deactivate-mark)
+          ad-do-it
+          (if (not forward)
+              (isearch-repeat-backward)
+            (goto-char (mark))
+            (isearch-repeat-forward)))
+      ad-do-it))
+  ;; push isearch search to project-find-regexp (C-x p g)
+  (defun isearch-rp-project ()
+    (interactive)
+    (let ((query (if isearch-regexp
+		     isearch-string
+		   (regexp-quote isearch-string))))
+      (isearch-update-ring isearch-string isearch-regexp)
+      (let (search-nonincremental-instead)
+        (ignore-errors (isearch-done t t)))
+      (project-find-regexp query)))
+  (defun isearch-rp-consult-line ()
+    "Invoke `consult-line' from isearch."
+    (interactive)
+    (let ((query (if isearch-regexp
+		     isearch-string
+		   (regexp-quote isearch-string))))
+      (isearch-update-ring isearch-string isearch-regexp)
+      (let (search-nonincremental-instead)
+        (ignore-errors (isearch-done t t)))
+      (consult-line query)))
+  :bind
+  (:map isearch-mode-map
+	("M-o" . isearch-occur)
+        ("M-p" . isearch-rp-project)
+	("M-." . isearch-forward-thing-at-point)
+	("M-l" . isearch-rp-consult-line)
+	;; ("C-j" . avy-isearch)
+	)
+  )
 
-  ;; set backup directory (Use copying to avoid symlinks)
-  (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
-	  backup-by-copying t
-	  delete-old-versions t
-	  kept-new-versions 6
-	  kept-old-versions 2
-	  version-control t)
-  (setq auto-save-file-name-transforms
-	`((".*" ,(concat user-emacs-directory "saves") t)))
-)
+(use-package elec-pair
+  :ensure nil
+  :config
+  (electric-pair-mode 1))
+
+(defun toggle-comment-on-line-or-region ()
+  "Toggle comment on the current line or active region."
+  (interactive)
+  (if (use-region-p)
+      (comment-or-uncomment-region (region-beginning) (region-end))
+    (comment-or-uncomment-region (line-beginning-position) (line-end-position))))
+
+(use-package emacs
+  :ensure nil
+  :init
+  ;; Set up keybindings for config workflow
+  (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Make esc work like C-g
+  (global-set-key (kbd "M-o") 'other-window)              ; `C-x o' is a 2 step key binding. `M-o' is much easier.
+  (global-set-key (kbd "C-;") 'toggle-comment-on-line)
+  (global-set-key (kbd "M-k") 'kill-current-buffer)
+
+  ;; zoom in and out
+  (global-set-key (kbd "C-=") 'text-scale-increase)
+  (global-set-key (kbd "C--") 'text-scale-decrease)
+  (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+  (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+
+  ;; config management
+  (global-set-key (kbd "M-s M-r")
+    (lambda () (interactive) (load-file "~/.config/emacs/init.el")))
+  (global-set-key (kbd "M-s M-c")
+    (lambda () (interactive) (find-file "~/.config/emacs/config.org")))
+  )
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace) ; Delete whitespace just when a file is saved.
+
+(use-package all-the-icons :ensure t)
+(use-package all-the-icons-completion :ensure t)
+(use-package all-the-icons-dired :ensure t)
 
 ;; (set-face-attribute 'variable-pitch nil
 ;; 		    :family "Ubuntu"
@@ -115,95 +206,76 @@
 	  ))
   :config (load-theme 'modus-vivendi))
 
-(defun toggle-comment-on-line ()
-  "Comment or uncomment current line."
-  (interactive)
-  (comment-or-uncomment-region (line-beginning-position) (line-end-position)))
+(use-package treesit
+    :ensure nil
+    :config
+    (setq treesit-font-lock-level 4)
+    (add-to-list 'treesit-language-source-alist '(python "https://github.com/tree-sitter/tree-sitter-python"))
+    (add-to-list 'treesit-language-source-alist '(javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+    (add-to-list 'treesit-language-source-alist '(typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+    (add-to-list 'treesit-language-source-alist '(tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+    (add-to-list 'treesit-language-source-alist '(html "https://github.com/tree-sitter/tree-sitter-html"))
+    (add-to-list 'treesit-language-source-alist '(css "https://github.com/tree-sitter/tree-sitter-css"))
+    (add-to-list 'treesit-language-source-alist '(elisp "https://github.com/Wilfred/tree-sitter-elisp"))
+    (add-to-list 'treesit-language-source-alist '(bash "https://github.com/tree-sitter/tree-sitter-bash"))
+    (add-to-list 'treesit-language-source-alist '(make "https://github.com/alemuller/tree-sitter-make"))
+    (add-to-list 'treesit-language-source-alist '(dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile" "main" "src"))
+    (add-to-list 'treesit-language-source-alist '(json "https://github.com/tree-sitter/tree-sitter-json"))
+    (add-to-list 'treesit-language-source-alist '(toml "https://github.com/tree-sitter/tree-sitter-toml"))
+    (add-to-list 'treesit-language-source-alist '(yaml "https://github.com/ikatyang/tree-sitter-yaml"))
+    (add-to-list 'treesit-language-source-alist '(cmake "https://github.com/uyha/tree-sitter-cmake"))
+    (use-package markdown-ts-mode
+      :ensure t
+      :mode ("\\.md\\'" . markdown-ts-mode)
+      :defer 't
+      :config
+      (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
+      (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src"))
+(defun my/markdown-ts-mode-hook ()
+  ;; Add the treesit font-lock settings for bash
+  (setq-local treesit-font-lock-settings
+              (append
+               treesit-font-lock-settings
+               `((bash . ((font-lock-keywords . ((,(regexp-quote "function") . font-lock-keyword-face)
+                                                 (,(regexp-quote "echo") . font-lock-builtin-face)
+                                                 ;; You can add more custom rules here for bash
+                                                 )))))))
 
-(use-package emacs
-  :init
-  ;; Set up keybindings for config workflow
-  ;; Make esc work like C-g
-  (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-  ;; `C-x o' is a 2 step key binding. `M-o' is much easier.
-  (global-set-key (kbd "M-o") 'other-window)
-  (global-set-key (kbd "C-;") 'toggle-comment-on-line)
-  (global-set-key (kbd "M-k") 'kill-current-buffer)
+  ;; Set up language injection rules for fenced code blocks with bash
+  (setq-local treesit-range-settings
+              (treesit-range-rules
+               :embed 'bash
+               :host 'markdown
+               :local t
+               :offset '(7 . -4)
+               '(((fenced_code_block) @cap (:match "```bash" @cap))))))
 
-  ;; zoom in and out
-  (global-set-key (kbd "C-=") 'text-scale-increase)
-  (global-set-key (kbd "C--") 'text-scale-decrease)
-  (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
-  (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+  ;; Ensure treesit is set up for markdown mode
+  (add-hook 'markdown-ts-mode-hook #'treesit-major-mode-setup))
 
-  (global-set-key (kbd "M-s M-r")
-    (lambda () (interactive) (load-file "~/.config/emacs/init.el")))
-  (global-set-key (kbd "M-s M-c")
-    (lambda () (interactive) (find-file "~/.config/emacs/config.org")))
-  )
+(add-hook 'markdown-ts-mode-hook #'my/markdown-ts-mode-hook)
+      )
 
-;; Delete whitespace just when a file is saved.
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; update isearch functionality
-(use-package isearch
-  :ensure nil
-  :defer t
-  :config
-  ;; adds a counter eg 4/34 to isearch
-  (setq isearch-lazy-count t)
-  ;; change isearch space literal to non greedy regex (this acts weird, doesn't mark the whole area)
-  ;; (setq search-whitespace-regexp "*.?")
-
-  ;; use selection to search (https://www.reddit.com/r/emacs/comments/2amn1v/comment/cixq7zx/)
-  (defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
-    (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
-        (progn
-          (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
-          (deactivate-mark)
-          ad-do-it
-          (if (not forward)
-              (isearch-repeat-backward)
-            (goto-char (mark))
-            (isearch-repeat-forward)))
-      ad-do-it))
-  ;; push isearch search to porject-find-regexp (C-x p g)
-  (defun isearch-rp-project ()
-    (interactive)
-    (let ((query (if isearch-regexp
-		     isearch-string
-		   (regexp-quote isearch-string))))
-      (isearch-update-ring isearch-string isearch-regexp)
-      (let (search-nonincremental-instead)
-        (ignore-errors (isearch-done t t)))
-      (project-find-regexp query)))
-  (defun isearch-rp-consult-line ()
-    "Invoke `consult-line' from isearch."
-    (interactive)
-    (let ((query (if isearch-regexp
-		     isearch-string
-		   (regexp-quote isearch-string))))
-      (isearch-update-ring isearch-string isearch-regexp)
-      (let (search-nonincremental-instead)
-        (ignore-errors (isearch-done t t)))
-      (consult-line query)))
-  :bind
-  (:map isearch-mode-map
-	("M-o" . isearch-occur)
-        ("M-p" . isearch-rp-project)
-	("M-." . isearch-forward-thing-at-point)
-	("M-l" . isearch-rp-consult-line)
-	;; ("C-j" . avy-isearch)
-	)
-  )
+    (dolist (source treesit-language-source-alist)
+      (unless (treesit-ready-p (car source))
+        (treesit-install-language-grammar (car source))))
+    )
 
 (use-package syntax-subword
+  :ensure t
   :config (global-syntax-subword-mode))
 
 ;; save minibuffer histories. Vertico uses to put recently selected options at the top.
 (savehist-mode 1)
 ;; save recently visited files. Consult uses it to put recent files options at the top.
 (recentf-mode 1)
+
+;; Adds out-of-order pattern matching algorithm
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless basic)))
+
 ;; Minibuffer live ui
 (use-package vertico
   :ensure t
@@ -225,6 +297,7 @@
 ;; Gives previews for current item
 ;; binds M-s as opposed to native C-s C-r
 (use-package consult
+  :ensure t
   :bind (;; A recursive grep
          ("M-s M-g" . consult-ripgrep)
 	 ("M-s M-G" . consult-grep)
@@ -254,31 +327,11 @@
 	  (apply (if vertico-mode
 		     #'consult-completion-in-region
 		   #'completion--in-region)
-		 args)))
-
-  (defvar consult-rp-project-files-source
-    (list :name     "Project Files"
-          :category 'file
-          :narrow   ?p
-          :face     'consult-file
-          :history  'file-name-history
-          :state    #'consult--file-state
-	  ;; :action   ,(lambda (file)
-	  ;; 	       (consult--file-action
-	  ;; 		(expand-file-name file (project-root (project-current)))))
-          :items
-	  ;; TODO: shorten the name and color me different
-	  (lambda ()
-	    (when-let* ((project (project-current))
-			(root (project-root project)))
-	      ;; (mapcar (lambda (file) (file-relative-name file root))
-	      (project-files project)))))
-
-  (add-to-list 'consult-buffer-sources 'consult-rp-project-files-source 'append)
-  )
+		 args))))
 
 ;; adds actions for current item
 (use-package embark
+  :ensure t
   :bind (("C-." . embark-act)
          :map minibuffer-local-map
          ("C-c C-c" . embark-collect)
@@ -286,11 +339,13 @@
 
 ;; adds embark actions to consult functions
 (use-package embark-consult
+  :ensure t
   :hook (embark-collect-mode . consult-preview-at-point-mode))
 
 ;; edit the results of a grep search  while inside a `grep-mode' buffer.
 ;; toggle editable mode, make changes, type C-c C-c to confirm | C-c C-k to abort.
 (use-package wgrep
+  :ensure t
   :bind ( :map grep-mode-map
           ("e" . wgrep-change-to-wgrep-mode)
           ("C-x C-q" . wgrep-change-to-wgrep-mode)
@@ -315,3 +370,40 @@
   :config
   (global-set-key (kbd "C-c f u") #'sudo-edit-find-file)
   (global-set-key (kbd "C-c f U") #'sudo-edit))
+
+(use-package undo-tree
+  :ensure t
+  :init
+  (global-undo-tree-mode)
+  :config
+  (setq undo-tree-history-directory-alist `(("." . ,(concat user-emacs-directory "undo")))))
+
+;; adds colors to delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :hook
+  (prog-mode . rainbow-delimiters-mode)
+  :config
+  (rainbow-delimiters-mode 1))
+
+;; adds colors to color indications e.g #fff000
+(use-package rainbow-mode :ensure t)
+
+;; M-x copilot-install-server
+;; M-x copilot-login
+(use-package copilot
+  :ensure nil
+  :vc (:url "https://github.com/copilot-emacs/copilot.el" :branch "main")
+  :init
+  (use-package dash :ensure t)
+  (use-package s :ensure t)
+  (use-package editorconfig :ensure t)
+  (use-package f :ensure t)
+  :bind (:map copilot-completion-map
+              ("<tab>" . copilot-accept-completion)
+              ("TAB" . copilot-accept-completion))
+  :hook (prog-mode . copilot-mode)
+  :config
+  (setq copilot-max-char -1)
+  (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode . 2))
+  (add-to-list 'copilot-indentation-alist '(org-mode . 2)))
