c394654837054aeeb6d382f3defd78e8c2a1caca
Perelman Roi (DI FA) <roi-perelman@siemens.com>
2025-07-13 15:21:28 +0300

    wezterm: allow wezterm tabs to work alongside tmux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,7 +23,20 @@ Config.tab_bar_at_bottom = true
 Config.use_fancy_tab_bar = false
 Config.tab_max_width = 25
 
--- -- tmux
+-- add tab Ctrl-Shift-t.
+Config.keys = {
+  { key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
+  { key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
+  { key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
+  { key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
+  { key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
+  { key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
+  { key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
+  { key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
+  { key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
+}
+
+-- -- tmux alternative option
 -- Config.leader = { key = "a", mods = "CTRL" }
 -- Config.keys = {
 -- 	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
